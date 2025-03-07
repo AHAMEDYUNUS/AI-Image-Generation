424401fb5c8e6e5fee010f6698cd804f1d27cf81
AHAMED YUNUS A <107417440+AHAMEDYUNUS@users.noreply.github.com>
2025-03-07 09:44:59 +0000

    completed

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -2,8 +2,69 @@ import React, { useContext } from 'react'
 import { assets, plans } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import {motion} from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 const BuyCredit = () => {
-  const {user} = useContext(AppContext)
+
+  //backendUrl = import.meta.env.VITE_BACKEND_URL
+  const {user, backendUrl, loadCreditsData, token, setShowLogin} = useContext(AppContext)
+
+  const initPay =  async (order) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Credits Payment',
+      description: 'Payment for credits',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+        try {
+          
+          const data = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/user/verify-razor`, 
+            response, {headers: {token}})
+
+            if (data.data.success) {  
+              await loadCreditsData();  
+              toast.success('Credits added');
+              navigate('/'); 
+            }
+
+        } catch (error) {
+          toast.error(error.message)
+        }
+      }
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+
+  }
+ 
+  const navigate = useNavigate();
+
+  const paymentRazorpay = async(planId)=>{
+    try {
+      if(!user){
+        setShowLogin(true)
+      }
+
+      const {data} = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/user/pay-razor`, {planId}, 
+        {headers: {token}})
+
+      if(data.success){
+        initPay(data.order)
+
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+      
+    }
+  }
+
+
   return (
     <motion.div 
     initial={{opacity:0.2, y:100}}
@@ -21,8 +82,8 @@ const BuyCredit = () => {
             <img width={40} src={assets.lock_icon} alt=''/>
             <p className='mt-3 mb-1 font-semibold ' >{item.id}</p>
             <p className='text-sm'>{item.desc}</p>
-            <p className='mt-6'><span className='text-3xl font-medium'>${item.price}</span>/{item.credits}credits</p>
-            <button className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52'>
+            <p className='mt-6'><span className='text-3xl font-medium'>₹{item.price}</span>/{item.credits}credits</p>
+            <button onClick={()=>paymentRazorpay(item.id)}className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52'>
             {user ? 'Purchase' : 'Get Started'}</button>
         </div>
     ))}
